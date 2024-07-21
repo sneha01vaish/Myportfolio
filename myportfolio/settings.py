f8702bdc58dd9136adabd1a3a43a16fba99a066d
Sneha Vaish <126096173+sneha01vaish@users.noreply.github.com>
2024-07-21 05:53:32 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static' )
